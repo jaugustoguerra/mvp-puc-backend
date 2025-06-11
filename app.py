81964c8963a988d497867864ee3749513c2f8cdd
Jose Augusto <splgugulive1@gmail.com>
2025-06-11 11:22:56 -0300

    versãof v1.0

--- app.py
+++ app.py
@@ -29,8 +29,10 @@ def cadastrar_aluno():
 
     con = sqlite3.connect(DB)
     cur = con.cursor()
-    cur.execute("INSERT INTO alunos (nome, email, data_nascimento) VALUES (?, ?, ?)",
-                (data['nome'], data['email'], data_nascimento_br))
+    cur.execute("""
+    INSERT INTO alunos (nome, email, data_nascimento, cpf)
+    VALUES (?, ?, ?, ?)
+""", (data['nome'], data['email'], data_nascimento_br, data['cpf']))
     con.commit()
     con.close()
 
@@ -43,24 +45,42 @@ def buscar_alunos():
     cur.execute("SELECT * FROM alunos")
     alunos = cur.fetchall()
     con.close()
-    return jsonify([{"id": a[0], "nome": a[1], "email": a[2], "data_nascimento": a[3]} for a in alunos])
+    return jsonify([
+        {
+            "id": a[0],
+            "nome": a[1],
+            "email": a[2],
+            "data_nascimento": a[3],
+            "cpf": a[4]
+        } for a in alunos
+    ])
 
-@app.route('/buscar_aluno/<int:id>', methods=['GET'])
-def buscar_aluno(id):
+
+@app.route('/buscar_aluno/<cpf>', methods=['GET'])
+def buscar_aluno_por_cpf(cpf):
     con = sqlite3.connect(DB)
     cur = con.cursor()
-    cur.execute("SELECT * FROM alunos WHERE id = ?", (id,))
+    cur.execute("SELECT * FROM alunos WHERE cpf = ?", (cpf,))
     aluno = cur.fetchone()
     con.close()
+
     if aluno:
-        return jsonify({"id": aluno[0], "nome": aluno[1], "email": aluno[2], "data_nascimento": aluno[3]})
+        return jsonify({
+            "id": aluno[0],
+            "nome": aluno[1],
+            "email": aluno[2],
+            "data_nascimento": aluno[3],
+            "cpf": aluno[4]
+        })
+
     return jsonify({"erro": "Aluno não encontrado"}), 404
 
-@app.route('/deletar_aluno/<int:id>', methods=['DELETE'])
-def deletar_aluno(id):
+
+@app.route('/deletar_aluno/<cpf>', methods=['DELETE'])
+def deletar_aluno(cpf):
     con = sqlite3.connect(DB)
     cur = con.cursor()
-    cur.execute("DELETE FROM alunos WHERE id = ?", (id,))
+    cur.execute("DELETE FROM alunos WHERE cpf = ?", (cpf,))
     con.commit()
     con.close()
     return jsonify({"mensagem": "Aluno excluído com sucesso"})
--- models.py
+++ models.py
@@ -4,12 +4,13 @@ def criar_tabela(db):
     con = sqlite3.connect(db)
     cur = con.cursor()
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS alunos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
-            email TEXT NOT NULL,
-            data_nascimento TEXT
-        )
-    """)
+    CREATE TABLE IF NOT EXISTS alunos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT NOT NULL,
+        data_nascimento TEXT,
+        cpf TEXT UNIQUE NOT NULL
+    )
+""")
     con.commit()
     con.close()
